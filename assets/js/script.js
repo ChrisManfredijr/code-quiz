70c739f7a646764d635abaeb3e9a14f905742730
Chris <chrismanfredijr@gmail.com>
2022-04-15 18:00:27 -0400

    quiz questions and styling added

---
+++ assets/js/script.js
@@ -0,0 +1,70 @@
+var questionTotal = 5;
+var questionCount = 1;
+var countDown = 30;
+var introWrapperEl = document.querySelector("#intro-wrapper");
+var navTimeEl = document.querySelector("#nav-time");
+var quizAreaEl = document.querySelector("#quiz-area"); 
+var quizQuestionEl = document.querySelector("#question"+ questionCount);
+var timer;
+
+
+var viewHighscores = function(event){
+   
+}
+
+var gameTimer = function(){
+    var gameTimer = document.querySelector("#game-timer");
+    var timer = setInterval(function(){
+        if(countDown === 0){
+            clearInterval(timer);
+            endGame();   
+        }else if(questionCount > questionTotal){
+            clearInterval(timer);
+        };
+        countDown--;
+        gameTimer.innerHTML = "Timer:" + countDown;
+    }, 1000);
+}
+
+var startGame = function(event) {
+    event.preventDefault();
+    var targetEl = event.target;
+   if(targetEl.matches(".btn")){
+      introWrapperEl.style.display = "none";
+      quizAreaEl.style.display = "flex";
+      playGame();
+      gameTimer();
+      };
+   }
+
+
+
+var playGame = function() {
+    if(questionCount <= questionTotal) {
+        var quizQuestionEl = document.querySelector("#question" + questionCount);
+        quizQuestionEl.style.display = "flex";
+    }else{
+        endGame();
+    };
+
+}
+var submitAnswer = function(event){
+    event.preventDefault();
+    var quizQuestionEl = document.querySelector("#question" + questionCount);
+    var targetEl = event.target;
+
+   if(targetEl.matches(".btn")){
+    quizQuestionEl.style.display = "none";
+    questionCount++;
+    playGame();
+   }
+ 
+}
+
+var endGame = function(){
+    clearInterval(timer);
+    quizAreaEl.style.display = "none";
+}
+introWrapperEl.addEventListener("click", startGame);
+navTimeEl.addEventListener("click", viewHighscores);
+quizAreaEl.addEventListener("click", submitAnswer);
